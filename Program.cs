ea88e4b88a1d81ba8df2a8d1b0e0e2e31c60fe7a
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-01-29 12:17:45 +0300

    Git init

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿// Console.Clear();
+// Console.WriteLine("Task 10");
+// Console.WriteLine("------");
+
+// Console.Write("Enter number:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine((num1%100)/10);
+
+// Console.ReadLine();
+
+// /*--------------------------------------------*/
+
+// Console.Clear();
+// Console.WriteLine("Task 13");
+// Console.WriteLine("------");
+
+// Console.Write("Enter number:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int numDigit = 1000;
+
+// if (num2 < 100) Console.WriteLine("No third digit");
+// else{
+//     while(num2>numDigit){
+//         numDigit *= 10;
+//     }
+
+//     numDigit/=100;
+//     num2 %= numDigit;
+//     numDigit/=10;
+//     num2 /= numDigit;
+//     Console.WriteLine(num2);
+// }
+
+// Console.ReadLine();
+
+// /*--------------------------------------------*/
+
+// Console.Clear();
+// Console.WriteLine("Task 15");
+// Console.WriteLine("------");
+
+// Console.Write("Enter number:");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// if (0<num3&& num3<=7){
+//     if(num3 > 5) Console.WriteLine("Yes");
+//     else
+//          Console.WriteLine("No");
+// }
+// else
+//     Console.WriteLine("Incorrect number");
+
+// Console.ReadLine();
+
+// /*--------------------------------------------*/
+
+Console.Clear();
+Console.WriteLine("Task Add");
+Console.WriteLine("------");
+
+Console.Write("Enter number:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max1 = 0;
+int max2 = 0;
+
+while (num3 != 0)
+{
+// программный код
+num3 = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.ReadLine();
